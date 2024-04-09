7f35721d3292b45100917aa4ed81502bab36fcf7
Adarsh Kaushal <adarshkaushal14@gmail.com>
2024-04-09 13:28:41 +0530

    deployment bugs fixes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,31 @@
 import connectToDB from "./database/database.js";
-import app from "./app.js";
 import 'dotenv/config'
+import path from 'path'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
+import problemRouter from './routes/problem.route.js';
 
+import cors from 'cors';
+
+const __dirname = path.resolve();
+const app = express();
+app.use(cors({
+    origin : '*'
+}))
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth" , authRouter);
+app.use("/api/user" , userRouter);
+app.use("/api/problem" , problemRouter);
+
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
+
+app.get("*" , (req , res) => {
+    res.sendFile(path.join(__dirname  ,  "frontend" , "dist" , "index.html"))
+})
 
 const port = process.env.PORT;
 connectToDB().then(app.listen(port , () => {
